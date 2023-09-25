407e9f4e47443e1f9ecc1d6aab6958426f868869
Phoenix0415 <phoenixx0415@gmail.com>
2023-09-25 16:54:42 +0930

    created 'To Do List.py', and added some comments on some files
    added some comments on file Dice Roll Generator.py and Countdown Clock and Timer.py. created a new file'To Do List.py'

--- beginner-projects/Countdown Clock and Timer.py
+++ beginner-projects/Countdown Clock and Timer.py
@@ -1,22 +1,28 @@
 '''
-Countdown Timer
+Countdown Timer Program:
 -------------------------------------------------------------
+This program demonstrates a simple countdown timer functionality.
+1. The user is prompted to enter the desired countdown time in seconds.
+2. The `countdown` function takes this time as an argument and initiates a countdown.
+3. Inside the function:
+    - A while loop iterates until the time reaches zero.
+    - The divmod function is used to calculate minutes and seconds from the total seconds.
+    - The formatted time is printed to the console on the same line (end='\r').
+    - The program sleeps for 1 second using time.sleep before decrementing the countdown time by 1.
+4. When the time reaches zero, a 'Lift off!' message is displayed.
 '''
 
-
-import time
-
+import time  # Importing the time module to use the sleep function.
 
 def countdown(user_time):
-   while user_time >= 0:
-       mins, secs = divmod(user_time, 60)
-       timer = '{:02d}:{:02d}'.format(mins, secs)
-       print(timer, end='\r')
-       time.sleep(1)
-       user_time -= 1
-   print('Lift off!')
-
+    while user_time >= 0:  # Loop continues until time reaches zero.
+        mins, secs = divmod(user_time, 60)  # Calculating minutes and seconds from the total seconds.
+        timer = '{:02d}:{:02d}'.format(mins, secs)  # Formatting the time in mm:ss format.
+        print(timer, end='\r')  # Printing the time on the same line.
+        time.sleep(1)  # Pausing the execution for 1 second.
+        user_time -= 1  # Decrementing the countdown time by 1.
+    print('Lift off!')  # Displaying the lift off message when the time reaches zero.
 
 if __name__ == '__main__':
-   user_time = int(input("Enter a time in seconds: "))
-   countdown(user_time)
+    user_time = int(input("Enter a time in seconds: "))  # Prompting the user to enter the countdown time in seconds.
+    countdown(user_time)  # Calling the countdown function with the user provided time as an argument.
--- beginner-projects/Dice Roll Generator.py
+++ beginner-projects/Dice Roll Generator.py
@@ -1,53 +1,59 @@
 '''
-Dice Roll Generator
+Dice Roll Generator Program:
 -------------------------------------------------------------
+This program simulates the rolling of one or two dice.
+1. The user is asked how many dice they want to roll.
+2. The `roll_dice` function is called to initiate the dice rolling process.
+3. Inside the function:
+    - A while loop continues the dice rolling process until the user decides to exit.
+    - The `num_die` function validates the user's input for the number of dice to roll.
+    - Depending on the user's choice, one or two dice are rolled, and the result is displayed.
+    - The user is then asked if they want to roll again.
 '''
 
-
-import random
-import os
-
+import random  # Importing the random module to generate random numbers.
+import os  # Importing the os module to clear the console.
 
 def num_die():
-  while True:
-      try:
-          num_dice = input('Number of dice: ')
-          valid_responses = ['1', 'one', 'two', '2']
-          if num_dice not in valid_responses:
-              raise ValueError('1 or 2 only')
-          else:
-              return num_dice
-      except ValueError as err:
-          print(err)
-
+    # This function validates the user input for the number of dice to roll.
+    while True:
+        try:
+            num_dice = input('Number of dice: ')
+            valid_responses = ['1', 'one', 'two', '2']
+            if num_dice not in valid_responses:
+                raise ValueError('1 or 2 only')  # Raises a ValueError if input is invalid.
+            else:
+                return num_dice  # Returns the valid input.
+        except ValueError as err:
+            print(err)  # Prints the error message.
 
 def roll_dice():
-   min_val = 1
-   max_val = 6
-   roll_again = 'y'
+    min_val = 1  # Minimum value of a die.
+    max_val = 6  # Maximum value of a die.
+    roll_again = 'y'  # Initial value to enter the while loop.
 
-   while roll_again.lower() == 'yes' or roll_again.lower() == 'y':
-       os.system('cls' if os.name == 'nt' else 'clear')
-       amount = num_die()
+    while roll_again.lower() == 'yes' or roll_again.lower() == 'y':  # Continues rolling until user decides to exit.
+        os.system('cls' if os.name == 'nt' else 'clear')  # Clears the console.
+        amount = num_die()  # Calls the num_die function to get the number of dice to roll.
 
-       if amount == '2' or amount == 'two':
-           print('Rolling the dice...')
-           dice_1 = random.randint(min_val, max_val)
-           dice_2 = random.randint(min_val, max_val)
+        if amount == '2' or amount == 'two':  # If two dice are to be rolled.
+            print('Rolling the dice...')
+            dice_1 = random.randint(min_val, max_val)  # Rolls the first die.
+            dice_2 = random.randint(min_val, max_val)  # Rolls the second die.
 
-           print('The values are:')
-           print('Dice One: ', dice_1)
-           print('Dice Two: ', dice_2)
-           print('Total: ', dice_1 + dice_2)
+            print('The values are:')
+            print('Dice One: ', dice_1)
+            print('Dice Two: ', dice_2)
+            print('Total: ', dice_1 + dice_2)  # Prints the total of two dice.
 
-           roll_again = input('Roll Again? ')
-       else:
-           print('Rolling the die...')
-           dice_1 = random.randint(min_val, max_val)
-           print(f'The value is: {dice_1}')
+            roll_again = input('Roll Again? ')
+        else:  # If one die is to be rolled.
+            print('Rolling the die...')
+            dice_1 = random.randint(min_val, max_val)  # Rolls the die.
 
-           roll_again = input('Roll Again? ')
+            print(f'The value is: {dice_1}')  # Prints the value of the die.
 
+            roll_again = input('Roll Again? ')
 
 if __name__ == '__main__':
-   roll_dice()
+    roll_dice()  # Calls the roll_dice function to initiate the process.
